85ad16adf28c87768f7f8f1637236bf61230a4bf
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-10-10 13:06:06 +0000

    adding mongoose

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import createError from "http-errors";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import mongoose from 'mongoose';
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/your-app-name');
 
 const app = express();
 
