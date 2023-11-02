6b04b789b861912e18e762c43bb5bd1158bf0146
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-11-02 17:07:39 +0000

    security update gardenController

--- controllers/gardenController.js
+++ controllers/gardenController.js
@@ -1,15 +1,33 @@
 import Garden from '../models/gardenModel.js'
+import { body, validationResult } from 'express-validator'
+
+// Middleware pour valider les données de jardin
+const validateGarden = [
+  body('name').trim().isLength({ min: 1 }).withMessage('Name is required'),
+  body('location').trim().isLength({ min: 1 }).withMessage('Location is required'),
+  // Ajoutez d'autres validations selon les champs de votre modèle Garden
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    next()
+  }
+]
 
 // Créer un nouveau jardin
-export const createGarden = async (req, res) => {
-  try {
-    const garden = new Garden(req.body)
-    const savedGarden = await garden.save()
-    res.status(201).json(savedGarden)
-  } catch (error) {
-    res.status(400).json({ message: error.message })
+export const createGarden = [
+  validateGarden,
+  async (req, res) => {
+    try {
+      const garden = new Garden(req.body)
+      const savedGarden = await garden.save()
+      res.status(201).json(savedGarden)
+    } catch (error) {
+      res.status(400).json({ message: 'Failed to create garden' })
+    }
   }
-}
+]
 
 // Récupérer tous les jardins
 export const getAllGardens = async (req, res) => {
@@ -17,7 +35,7 @@ export const getAllGardens = async (req, res) => {
     const gardens = await Garden.find().populate('plants')
     res.json(gardens)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Failed to retrieve gardens' })
   }
 }
 
@@ -30,50 +48,48 @@ export const getGardenById = async (req, res) => {
     }
     res.json(garden)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Failed to retrieve garden' })
   }
 }
 
 // Mettre à jour un jardin
-export const updateGarden = async (req, res) => {
-  try {
-    const id = req.params.id
-    const garden = await Garden.findById({ _id: { $eq: id } })
-    if (!garden) {
-      return res.status(404).json({ message: 'Garden not found' })
+export const updateGarden = [
+  validateGarden,
+  async (req, res) => {
+    try {
+      const updatedGarden = await Garden.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      if (!updatedGarden) {
+        return res.status(404).json({ message: 'Garden not found' })
+      }
+      res.json(updatedGarden)
+    } catch (error) {
+      res.status(400).json({ message: 'Failed to update garden' })
     }
-    await garden.update(req.body)
-    res.json(garden)
-  } catch (error) {
-    res.status(400).json({ message: error.message })
   }
-}
+]
 
 // Supprimer un jardin
 export const deleteGarden = async (req, res) => {
   try {
-    const id = req.params.id
-    const garden = await Garden.findByIdAndDelete({ _id: { $eq: id } })
-    if (!garden) {
+    const deletedGarden = await Garden.findByIdAndDelete(req.params.id)
+    if (!deletedGarden) {
       return res.status(404).json({ message: 'Garden not found' })
     }
-    res.status(204).json({ message: 'Garden deleted' })
+    res.status(204).send()
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Failed to delete garden' })
   }
 }
 
-// **Lister les plantes d'un jardin (gardenId)** : Cette fonction renvoie la liste des plantes associées à un jardin donné en utilisant son identifiant (gardenId).
+// **Lister les plantes d'un jardin (gardenId)**
 export const listPlantsInGarden = async (req, res) => {
   try {
-    const id = req.params.id
-    const garden = await Garden.findById({ _id: { $eq: id } }).populate('plants')
+    const garden = await Garden.findById(req.params.id).populate('plants')
     if (!garden) {
       return res.status(404).json({ message: 'Garden not found' })
     }
-    const plants = garden.plants // Récupérer la liste des plantes du jardin
-    res.json(plants)
+    res.json(garden.plants)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Failed to list plants' })
   }
 }
