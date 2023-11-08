65f0217b0ba5a99811a7df72a1c14cb00954b8d1
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-11-08 14:53:56 +0000

    update api-doc name

--- config/swagger.js
+++ config/swagger.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Hello World API',
+      title: 'Home Garden API',
       version: '1.0.0',
       description: 'A simple Express API'
     },
