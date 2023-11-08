13c338f4aff992e80dd697df285d1010f6892e40
CarolineH <73394670+CaroH502@users.noreply.github.com>
2023-11-08 09:55:42 +0000

    Ajout de la doc swagger pour création garden

--- routes/gardenRoutes.js
+++ routes/gardenRoutes.js
@@ -12,6 +12,75 @@ import { verifyToken } from '../services/authService.js'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Garden:
+ *       type: object
+ *       required:
+ *         - name
+ *         - location
+ *         - plants
+ *         - user
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Le nom du jardin
+ *         location:
+ *           type: Point
+ *           coordinates: [longitude, latitude]
+ *           description: La localisation du jardin en coordonnées GPS
+ *         plants:
+ *          type: Array
+ *          description: Les plantes du jardin
+ *         user:
+ *           type: string
+ *           description: L'utilisateur du jardin
+ *       example:
+ *         name: potagé
+ *         location: { type: Point, coordinates: [41.40338, 2.17403]}
+ *         plants: [Ocimum basilicum, Mentha spicata, Rosmarinus officinalis]
+ *         user: John Doe
+ */
+
+/**
+ * @openapi
+ * /api/gardens/register:
+ *   post:
+ *     tags:
+ *       - Gardens
+ *     summary: Register a new garden
+ *     description: This route allows you to register a new garden.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - location
+ *               - plants
+ *               - user
+ *             properties:
+ *               name:
+ *                 type: string
+ *               location:
+ *                 type: string
+ *               plants:
+ *                 type: string
+ *               user:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Graden registered successfully.
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal Server Error.
+ */
+
 // Route pour créer un nouveau jardin
 router.post('/', verifyToken, createGarden)
 
